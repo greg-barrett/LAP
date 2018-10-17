2d31e992521b7451a6775a931c2e1eff95f07758
Gregory Barrett <gbarrett15@gmail.com>
2018-10-17 13:59:16 +0200

    add devise for admin user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   root "static_pages#home"
   get '/about', to: "static_pages#about"
   get '/house', to: "static_pages#house"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181016095456) do
+ActiveRecord::Schema.define(version: 20181017115534) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "username", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_admins_on_username", unique: true
+  end
 
   create_table "reservations", force: :cascade do |t|
     t.date "arrival_date"
