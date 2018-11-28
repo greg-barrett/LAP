725b59d9599ac34cc858ed20f027b61db60966cd
Gregory Barrett <gbarrett15@gmail.com>
2018-11-28 16:46:05 +0100

    push to heroku-1

--- db/migrate/20181017123516_add_email_to_admins.rb
+++
@@ -1,7 +0,0 @@
-class AddEmailToAdmins < ActiveRecord::Migration[5.1]
-  def change
-    add_column :admins, :email, :string, null: false, default: ""
-    remove_column :admins, :username
-    add_index :admins, :email, unique: true
-  end
-end
